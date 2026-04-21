21d8eda79de24f134133ed32ac980a14542fa5ec
Eduarda Guimarães Monteiro <eduarda2005gmonteiro@gmail.com>
2026-04-21 14:26:51 -0300

    Services finalizado

--- models/usuario.py
+++ models/usuario.py
@@ -1,10 +1,6 @@
 from endereco import Endereco
 
 class Usuario:
-    """
-    Classe Usuario
-    Responsável por autenticação e controle de acesso ao sistema.
-    """
 
     def __init__(self, id_usuario, nome, data_nascimento, cpf, genero, telefone, email, senha, endereco, cargo, ativo=True): # login se dá por cpf e senha
         self.id_usuario = id_usuario
@@ -20,10 +16,6 @@ class Usuario:
         self.ativo = ativo
 
     def autenticar(self, cpf, senha):
-        """
-        Verifica se o usuário pode se autenticar no sistema.
-        Retorna True se as credenciais estiverem corretas e o usuário ativo.
-        """
         if not self.ativo:
             return False
 
@@ -33,13 +25,9 @@ class Usuario:
         return False
 
     def alterarSenha(self, nova_senha):
-        """
-        Altera a senha do usuário.
-        """
+
         self.senha = nova_senha
 
     def desativar(self):
-        """
-        Desativa o usuário no sistema.
-        """
+
         self.ativo = False
